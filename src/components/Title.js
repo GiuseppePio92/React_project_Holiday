09f78a4bb9a736244bd94f9e2afa2999beb3eb99
Giuseppe Pio <piogiuseppe.it@gmail.com>
2023-06-24 22:24:05 +0100

    holiday-project

---
+++ src/components/Title.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const titleStyle = {
+  width: 'fit-content',
+  fontVariant: 'samll-caps',
+  position: 'relative',
+  display: 'grid',
+  placeItems: 'center'
+}
+
+const Title = ({text}) => {
+  return <div style={titleStyle}>
+    <h4>{text || 'Titolo'}</h4>
+    <div className="underline"></div>
+  </div>;
+};
+
+export default Title;
